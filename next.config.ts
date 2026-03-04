32d1cbb42dc6ddb00796a59cf57bf7b1c9894447
Hi, ich bin Rico <4007513+loschke@users.noreply.github.com>
2026-03-04 12:50:51 +0100

    fix: unsafe eval

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              `script-src 'self' 'unsafe-inline'${isDev ? " 'unsafe-eval'" : ""}`,
+              "script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: blob: https:",
               "font-src 'self' data:",
